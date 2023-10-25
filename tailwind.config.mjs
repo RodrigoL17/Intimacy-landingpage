d5ee39ff16a977964a3012eca1fb6467728546f8
Rodrigo Lezama <cordolezama@gmail.com>
2023-10-25 10:53:14 -0300

    configuracion + imagenes

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				primary: ['Noto Serif Display', 'serif'],
+			}
+		},
+	},
+	plugins: [],
+}
